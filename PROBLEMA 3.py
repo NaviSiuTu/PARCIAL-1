b5f424f1dc5a6438437194fa971cad3e7bf29205
Your Name <isantisteban@unal.edu.co>
2025-05-07 12:59:41 -0500

    solucion 3

--- PROBLEMA 3.py
+++ PROBLEMA 3.py
@@ -1,8 +1,19 @@
-def contadorDeVocales(palabra):
-    A = 1
-    a = 1
-
+def contar_vocales(palabra): #Defne la función
     
+    # Solicita al usuario una palabra y cuenta cuántas vocales contie
+    
+    vocales = "aeiouAEIOU"
+    contador = 0
+    for letra in palabra:
+        if letra in vocales:
+            contador += 1
+    return contador
 
-palabra = (input("Escriba una palabra: "))
-print (contadorDeVocales(palabra))
+# Interaccion usuario con la consola
+palabra = input("Ingrese una palabra: ")
+
+# Cuenta las vocales en la palabra
+numero_vocales = contar_vocales(palabra)
+
+# Imprime el resultado
+print(f"La palabra '{palabra}' tiene {numero_vocales} vocales.")
